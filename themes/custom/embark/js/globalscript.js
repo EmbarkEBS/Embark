f7a1b3ac035ed5b2a18e759d5385d604e4a75585
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2023-07-11 10:48:37 +0530

    Update globalscript.js

--- themes/custom/embark/js/globalscript.js
+++ themes/custom/embark/js/globalscript.js
@@ -726,8 +726,8 @@ if ($(window).scrollTop() > 100) {
 }
 function makeTimer() {
 
-	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-		var endTime = new Date("29 April 2024 9:56:00 GMT+01:00");			
+	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
+		var endTime = new Date("29 April 2024 9:56:00 GMT+01:00");
 			endTime = (Date.parse(endTime) / 1000);
 
 			var now = new Date();
@@ -735,11 +735,11 @@ function makeTimer() {
 
 			var timeLeft = endTime - now;
 
-			var days = Math.floor(timeLeft / 86400); 
+			var days = Math.floor(timeLeft / 86400);
 			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
 			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
 			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-  
+
 			if (hours < "10") { hours = "0" + hours; }
 			if (minutes < "10") { minutes = "0" + minutes; }
 			if (seconds < "10") { seconds = "0" + seconds; }
@@ -747,8 +747,39 @@ function makeTimer() {
 			$("#days").html(days + "<span>Days</span>");
 			$("#hours").html(hours + "<span>Hours</span>");
 			$("#minutes").html(minutes + "<span>Minutes</span>");
-			$("#seconds").html(seconds + "<span>Seconds</span>");		
+			$("#seconds").html(seconds + "<span>Seconds</span>");
 
 	}
 
 	setInterval(function() { makeTimer(); }, 1000);
+
+
+  //landing page 1
+  function makeTimer1() {
+
+    //		var endTime = new Date("29 August 2023 9:56:00 GMT+01:00");
+      var endTime = new Date("29 August 2023 9:56:00 GMT+01:00");
+        endTime = (Date.parse(endTime) / 1000);
+
+        var now = new Date();
+        now = (Date.parse(now) / 1000);
+
+        var timeLeft = endTime - now;
+
+        var days = Math.floor(timeLeft / 86400);
+        var hours= Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+        var seconds = Math.floor((timeLeft- (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+
+        $("#days1").html(days + "<span>Days</span>");
+        $("#hours1").html(hours + "<span>Hours</span>");
+        $("#minutes1").html(minutes + "<span>Minutes</span>");
+
+
+    }
+
+    setInterval(function() { makeTimer1(); }, 1000);
